0cd428cce25b04a934621335605d7964cc98dea5
Luis Fernando <fernandoluis270@gmail.com>
2026-01-20 21:39:25 -0400

    importando arquivo de rotas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import contact from './routes/contactrouter.js'
 
 const app = express()
 const port = 3000
+
+app.use(contact)
 
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`)
