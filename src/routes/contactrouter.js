9d6601b5a6a3356fef82420f76bc9589dc3dd74d
Luis Fernando <fernandoluis270@gmail.com>
2026-01-21 17:07:15 -0400

    definindo rotas para os novos controllers

--- src/routes/contactrouter.js
+++ src/routes/contactrouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createContact, listContacts } from "../controllers/contactcontroller.js";
+import { createContact, deleteContact, listContacts, updateContact } from "../controllers/contactcontroller.js";
 
 const contact = Router()
 
 contact.get('/contatos', listContacts)
 contact.post('/contatos', createContact)
+contact.patch('/contatos/:id', updateContact)
+contact.delete('/contatos/:id', deleteContact)
 
 export default contact
